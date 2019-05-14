5b5543d1d596f7afaa2ebb50c7d90b29ca867b72
Victor Boctor <victor@mantishub.net>
2019-05-13 19:54:03 -0700

    Implement script to generate csv from strings

---
+++ generate_csv_from_strings.php
@@ -0,0 +1,41 @@
+<?php
+if( $argc < 2 ) {
+    $english_strings_file = dirname( $argc[0] ) . '/strings_english.txt';
+} else {
+    $english_strings_file = $argv[1];
+}
+
+if( !file_exists( $english_strings_file ) ) {
+    echo "File '$english_strings_file' not found.\n";
+    exit;
+}
+
+include( realpath( $english_strings_file ) );
+
+$vars = get_defined_vars();
+$strings = [];
+
+foreach( $vars as $name => $value ) {
+    if( stripos( $name, 's_' ) !== 0 ) {
+        continue;
+    }
+
+    $string_name = substr( $name, 2 );
+    $strings[$string_name] = $value;
+}
+
+$output_file_path = 'strings_english.csv';
+if( file_exists( $output_file_path ) ) {
+    unlink( $output_file_path );
+}
+
+if( !empty( $strings ) ) {
+    $fp = fopen( $output_file_path, 'w' );
+
+    foreach ( $strings as $name => $value ) {
+        fputcsv( $fp, [ $name, $value ] );
+    }
+
+    fclose($fp);
+}
+
